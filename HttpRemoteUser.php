42e6f39b6c2732f495d3749febb45c54ae496e70
Christian Holzberger <ch@mosaiksoftware.de>
2019-05-25 04:35:56 +0200

    Inital commit

---
+++ HttpRemoteUser.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of the Kimai CustomCSSBundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace KimaiPlugin\HttpRemoteUser;
+use App\Plugin\PluginInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HttpRemoteUser extends Bundle implements PluginInterface
+{
+}
